390ed85dc427a90f41aa9550b08a7194af8e9960
YouKnowWhoII <126054990+YouKnowWhoII@users.noreply.github.com>
2024-05-29 23:56:29 +0530

    Added functionality to get medical records as a text file

--- Fiber/routes/medicalrecordRoutes.go
+++ Fiber/routes/medicalrecordRoutes.go
@@ -9,6 +9,8 @@ func RegisterMedicalRecordRoutes(app *fiber.App) {
 	app.Post("/medicalrecords", handlers.CreateMedicalRecord)
 	app.Get("/medicalrecords", handlers.GetMedicalRecords)
 	app.Get("/medicalrecords/:id", handlers.GetMedicalRecord)
+	//route to return a text file with a medical record by id
+	app.Get("/medicalrecords/:id/download", handlers.GetMedicalRecordFile)
 	app.Put("/medicalrecords/:id", handlers.UpdateMedicalRecord)
 	app.Delete("/medicalrecords/:id", handlers.DeleteMedicalRecord)
 }
