606ca6dfd46fcb81549e0a5349ba436b4c4cba32
YouKnowWhoII <126054990+YouKnowWhoII@users.noreply.github.com>
2024-05-30 00:35:49 +0530

    Added rate limiting by IP

--- Fiber/routes/downloadRoutes.go
+++ Fiber/routes/downloadRoutes.go
@@ -2,10 +2,15 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"time"
 )
 
 func RegisterDownloadRoutes(app *fiber.App) {
-	app.Get("/download/manual.txt", func(c *fiber.Ctx) error {
+	app.Get("/download/manual.txt", limiter.New(limiter.Config{
+		Max:        10,
+		Expiration: 1 * time.Hour,
+	}), func(c *fiber.Ctx) error {
 		return c.Download("./public/manual.pdf")
 	})
 }
--- Fiber/routes/medicalrecordRoutes.go
+++ Fiber/routes/medicalrecordRoutes.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"GO/Fiber/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"time"
 )
 
 func RegisterMedicalRecordRoutes(app *fiber.App) {
@@ -10,7 +12,10 @@ func RegisterMedicalRecordRoutes(app *fiber.App) {
 	app.Get("/medicalrecords", handlers.GetMedicalRecords)
 	app.Get("/medicalrecords/:id", handlers.GetMedicalRecord)
 	//route to return a text file with a medical record by id
-	app.Get("/medicalrecords/:id/download", handlers.GetMedicalRecordFile)
+	app.Get("/medicalrecords/:id/download", limiter.New(limiter.Config{
+		Max:        5, // limit each IP to 5 requests per minute
+		Expiration: 1 * time.Minute,
+	}), handlers.GetMedicalRecordFile)
 	app.Put("/medicalrecords/:id", handlers.UpdateMedicalRecord)
 	app.Delete("/medicalrecords/:id", handlers.DeleteMedicalRecord)
 }
